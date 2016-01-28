6a44a3e71646123a9b4f5d4ca9ae6edbf17c1ede
ypxing <ypxing@gmail.com>
2016-01-28 12:01:59 +1100

    add source-map and hot replacement

--- src/histogram.jsx
+++ src/histogram.jsx
@@ -7,6 +7,10 @@ import Coordinate from "./coordinate"
 
 export default class Histogram extends React.Component {
   _d3_render() {
+    if (this.bar) {
+      this.bar.remove();
+    }
+
     // A formatter for counts.
     var formatCount = d3.format(",.0f");
     var svg = d3.select(this.refs.histogram);
@@ -14,23 +18,27 @@ export default class Histogram extends React.Component {
     var height = d3.max(props.yScale.range());
     var data = props.data;
 
-    var bar = svg.selectAll(".bar")
+    this.bar = svg.selectAll(".bar")
         .data(data)
       .enter().append("g")
         .attr("class", "bar")
         .attr("transform", function(d) { return "translate(" + props.xScale(d.x) + "," + props.yScale(d.y) + ")"; });
 
-    bar.append("rect")
+    this.bar.append("rect")
         .attr("x", 1)
         .attr("width", props.xScale(data[0].dx) - 1)
         .attr("height", function(d) { return height - props.yScale(d.y); });
 
-    // bar.append("text")
-    //     .attr("dy", ".75em")
-    //     .attr("y", 6)
-    //     .attr("x", props.xScale(data[0].dx) / 2)
-    //     .attr("text-anchor", "middle")
-    //     .text(function(d) { return formatCount(d.y); });
+    this.bar.append("text")
+        .attr("dy", ".75em")
+        .attr("y", 6)
+        .attr("x", props.xScale(data[0].dx) / 2)
+        .attr("text-anchor", "middle")
+        .text(function(d) { return formatCount(d.y); });
+  }
+
+  componentDidUpdate() {
+    this._d3_render();
   }
 
   componentDidMount() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
 var path = require("path")
 
 module.exports = {
-  entry: path.join(__dirname, "src", "entry.js"),
+  entry: ['webpack-dev-server/client?http://0.0.0.0:3003', 'webpack/hot/only-dev-server', path.join(__dirname, "src", "entry.js")],
   output: {
-      path: path.join(__dirname, "build"),
+      path: path.resolve(__dirname, "build"),
       filename: "bundle.js"
   },
   resolve: {
     extensions: ['', '.coffee', '.js', '.jsx']
   },
+  devtool: 'source-map',
   module: {
     loaders: [
       { test: /\.css$/, loader: "style!css" },
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel?presets[]=react,presets[]=es2015'
+        include: path.join(__dirname, 'src'),
+        loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
       }
     ]
   }
