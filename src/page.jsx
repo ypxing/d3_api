b712329f44b707471083d87fd6a7933d4310629a
ypxing <ypxing@gmail.com>
2016-02-02 15:13:17 +1100

    add rect to Page

--- src/page.jsx
+++ src/page.jsx
@@ -7,6 +7,7 @@ export default class Page extends React.Component {
         className="svg-content-responsive" preserveAspectRatio="xMinYMin meet"
         viewBox={`0 0 ${this.props.vbWidth || 1123} ${this.props.vbHeight || 794}`}>
         {this.props.children}
+        <rect x="0" y="0" width="1122" height="791" style={{stroke: "#000", strokeWidth: "1px", fill: "none"}}></rect>
       </svg>
     )
   }
